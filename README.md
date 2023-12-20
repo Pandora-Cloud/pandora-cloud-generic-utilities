81cf1a7a8b3b7b279940c1d87821391046c4aea5
Isi Lawson <ilawson@canojatech.com>
2023-12-20 20:17:18 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,49 @@
-# pandora-cloud-generic-utilities
+# Pandora Cloud Generic Utilities
+
+Welcome to the Pandora Cloud Generic Utilities repository. This repository contains a collection of miscellaneous tools and scripts designed to enhance and simplify various cloud-related operations. Each utility in this repository serves a specific purpose, making it easier to manage and automate tasks in cloud environments.
+
+## Utilities
+
+### AWS Security Group SSH Access Updater (`aws_security_group_ssh_access_updater.py`)
+
+The `aws_security_group_ssh_access_updater.py` script is a Python utility designed to update an AWS Security Group to allow SSH access from the user's current public IP address. This is particularly useful for dynamically updating security groups to ensure secure and temporary SSH access to AWS resources.
+
+#### Features
+
+- Automatically fetches the user's current public IP address.
+- Updates the specified AWS Security Group to allow SSH access from this IP address.
+- Removes any existing SSH (port 22) access rules from the security group before adding the new rule.
+
+#### Prerequisites
+
+- Python 3.x
+- AWS CLI configured with the necessary AWS credentials and permissions.
+- `boto3` (AWS SDK for Python).
+
+#### Usage
+
+Run the script with the security group ID as an argument. Optionally, set the `AWS_PROFILE` environment variable to use a specific AWS profile:
+
+```bash
+export AWS_PROFILE=myawsprofile  # Optional: Set this if using a specific AWS profile
+python aws_security_group_ssh_access_updater.py --security_group_id [security-group-id] --aws_profile [aws-profile-name]
+```
+
+Replace [security-group-id] with the actual ID of the AWS security group you wish to update.
+Replace [aws-profile-name] with the name of the AWS Profile with permission to perform the update.
+
+
+#### Scheduling with Cron
+
+To ensure your SSH access is always up-to-date with your current IP address, you can schedule the `aws_security_group_ssh_access_updater.py` script to run automatically at regular intervals using `cron`. This is especially useful if your public IP changes frequently.
+
+##### Steps to Schedule the Script:
+
+1. **Add a cron job:**
+
+    You can edit the crontab for the current user using the following command:
+
+    ```bash
+    crontab -e
+    0 7 * * * /usr/bin/python3 /path/to/aws_security_group_ssh_access_updater.py --security_group_id [security-group-id] --aws_profile [aws-profile-name] >> /path/to/logfile.log 2>&1
+    ```
---
+++ update_security_group_myip.py
@@ -0,0 +1,89 @@
+import subprocess
+import boto3
+import argparse
+import os
+
+def update_security_group_rule(security_group_id):
+    """
+    Update the specified security group to allow SSH access from the current public IP address.
+
+    This function performs the following steps:
+    1. Fetches the current public IP address.
+    2. Formats the IP address in CIDR notation.
+    3. Checks the existing rules in the specified security group.
+    4. Removes any existing rule that allows SSH access.
+    5. Adds a new rule to allow SSH access from the current IP address.
+
+    Args:
+    security_group_id (str): The ID of the AWS security group to update.
+
+    Returns:
+    None
+    """
+    # Use 'dig' command to get the current public IP address
+    try:
+        my_ip = subprocess.check_output(
+            ["dig", "+short", "myip.opendns.com", "@resolver1.opendns.com"],
+            encoding='utf-8'
+        ).strip()
+    except subprocess.CalledProcessError as e:
+        print(f"Failed to obtain public IP address: {e}")
+        return
+
+    cidr_ip = f"{my_ip}/32"
+
+    # Create a Boto3 client for EC2
+    ec2 = boto3.client('ec2')
+
+    try:
+        # Describe the security group
+        sg = ec2.describe_security_groups(GroupIds=[security_group_id])['SecurityGroups'][0]
+
+        # Find and remove the existing rule for SSH (port 22)
+        for permission in sg['IpPermissions']:
+            if permission['FromPort'] == 22 and permission['ToPort'] == 22 and permission['IpProtocol'] == 'tcp':
+                ec2.revoke_security_group_ingress(
+                    GroupId=security_group_id,
+                    IpPermissions=[permission]
+                )
+
+        # Add a new rule to the security group
+        ec2.authorize_security_group_ingress(
+            GroupId=security_group_id,
+            IpProtocol='tcp',
+            FromPort=22,
+            ToPort=22,
+            CidrIp=cidr_ip
+        )
+        print(f"Successfully updated rule in {security_group_id} allowing SSH access from {cidr_ip}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+def main():
+    """
+    Main function to process the command line arguments and update the security group.
+
+    This function uses argparse to parse command line arguments for the AWS profile
+    and the security group ID. It then sets the AWS profile and calls the
+    update_security_group_rule function with the security group ID.
+
+    Command Line Args:
+    --aws_profile (str): The AWS profile to use for the session.
+    --security_group_id (str): The ID of the AWS security group to update.
+
+    Returns:
+    None
+    """
+    parser = argparse.ArgumentParser(description='Update Security Group to allow SSH from current IP.')
+    parser.add_argument('--aws_profile', type=str, required=True, help='AWS profile to use')
+    parser.add_argument('--security_group_id', type=str, required=True, help='Security Group ID to be updated')
+    args = parser.parse_args()
+
+    # Set the AWS_PROFILE environment variable
+    os.environ['AWS_PROFILE'] = args.aws_profile
+
+    # Update the security group rule
+    update_security_group_rule(args.security_group_id)
+
+if __name__ == "__main__":
+    main()
